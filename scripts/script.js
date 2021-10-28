0e53ac1caaa39a5caca7bda519893c1a5fd0f429
kergma@gmail.com <kergma@gmail.com>
2021-10-28 10:59:33 +0300

    eng rus ukr fra v0.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,60 @@
 
 const contLangs = [
-    // { example:
+    // { //example:
     //     name: < 'id element by page html' >,
     //     property: < 'property name'>
-    //     eng: < 'textContent in eng lang' >,
-    //     ru: < 'textContent in ru lang' >,
-    //     ukr: < 'textContent in ukr lang' >,
+    //     ENG: < 'textContent in eng lang' >,
+    //     RUS: < 'textContent in ru lang' >,
+    //     UKR: < 'textContent in ukr lang' >,
     // }, 
     {
         name: 'title-site',
         property:'textContent',
-        eng: 'Register particent',
-        ru: 'Регистрация участников',
-        ukr: 'Рэiстрацiя участникiв',
+        ENG: 'Register particent',
+        RUS: 'Регистрация участников',
+        UKR: 'Рэiстрацiя участникiв',
+        FRA: 'Inscrivez-vous',
     },
+    {
+        name: 'h1-head',
+        property: 'textContent',
+        ENG: 'Hello All!',
+        RUS: 'Привет Всем!',
+        UKR: 'Привiт усiм!',
+        FRA: 'Bonjour à tous!'
+    },
+    {
+        name: 'btn-lang-rus',
+        property: 'textContent',
+        ENG: 'RUS',
+        RUS: 'РУС',
+        UKR: 'РОС',
+        FRA: 'RUS',
+    },
+    {
+        name: 'btn-lang-ukr',
+        property: 'textContent',
+        ENG: 'UKR',
+        RUS: 'УКР',
+        UKR: 'УКР',
+        FRA: 'UKR',
+    },
+    {
+        name: 'btn-lang-eng',
+        property: 'textContent',
+        ENG: 'ENG',
+        RUS: 'АНГ',
+        UKR: 'АНГ',
+        FRA: 'ANG',
+    },
+    // {
+    //     name: 'btn-lang-fra',
+    //     property: 'textContent',
+    //     ENG: 'FRA',
+    //     RUS: 'ФРА',
+    //     UKR: 'ФРА',
+    //     FRA: 'FRA',
+    // },
 ];
 
 function switchToLang(lang) {
@@ -21,6 +62,17 @@ function switchToLang(lang) {
         document.getElementById(x.name)[x.property] = x[lang]
     });
 }
+// init set site
+ var langSite = 'RUS';
+ switchToLang(langSite);
 
-var langSite = 'ru';
-switchToLang(langSite);
+
+
+document.querySelectorAll('.btn-head').forEach(x => {
+ x.addEventListener('click', function () {
+     document.querySelector('.btn-head-active')
+         .classList.remove('btn-head-active');
+  switchToLang(x.name);
+  x.classList.add('btn-head-active');
+ });
+});
